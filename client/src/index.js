b0d8fffde5eddac296f8d21fff7ec761aa46dfbb
nolan4509 <nolanm4509@gmail.com>
2018-09-17 16:05:27 -0400

    Beautify Package Overhaul
    Recently Installed Atom's Beautify Package, applied it to all front-end files to clean up code somewhat and reach a standard style.

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 //import 'bootstrap/dist/css/bootstrap-theme.css';
-import { render } from 'react-dom';
+import {render} from 'react-dom';
 import Main from './routes';
-import { BrowserRouter } from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 //import registerServiceWorker from './registerServiceWorker';
 //(at bottom) registerServiceWorker();
 
-render((
-    <BrowserRouter>
-        <Main />
-    </BrowserRouter>
-), document.getElementById('root'));
-
-
+render((<BrowserRouter>
+    <Main/>
+</BrowserRouter>), document.getElementById('root'));
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,15 +1,13 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import {Switch, Route} from 'react-router-dom'
 import Home from './home'
 import Login from './login'
 
-const Main = () => (
-    <main>
-        <Switch>
-            <Route exact path='/' component={Login}/>
-            <Route path='/home' component={Home}/>
-        </Switch>
-    </main>
-)
+const Main = () => (<main>
+    <Switch>
+        <Route exact="exact" path='/' component={Login}/>
+        <Route path='/home' component={Home}/>
+    </Switch>
+</main>)
 
 export default Main;
