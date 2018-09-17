fa9ad962f63017ec79218d4154a6341325197daa
nolan4509 <nolanm4509@gmail.com>
2018-09-17 17:34:49 -0400

    npm install jquery popper.js

--- client/src/home.js
+++ client/src/home.js
@@ -79,13 +79,14 @@ class Home extends Component {
     render() {
         return (<div>
             {/* Home */}
-
-            {/* Required meta tags */}
-            <meta charSet="utf-8"/> {/* <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" /> */}
-            {/* Bootstrap CSS */}
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous"/>
-            <link rel="stylesheet" href="styles.css"/>
-            <title>Home Page</title>
+            <head>
+                {/* Required meta tags */}
+                <meta charSet="utf-8"/> {/* <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" /> */}
+                {/* Bootstrap CSS */}
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous"/>
+                <link rel="stylesheet" href="styles.css"/>
+                <title>Home Page</title>
+            </head>
             {/* Home Page to be infinite scrolling, subcategories will have pagination */}
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <a className="navbar-brand" href="Home.html">Fuck the Microwave</a>
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 //import 'bootstrap/dist/css/bootstrap-theme.css';
 import {render} from 'react-dom';
 import Main from './routes';
 import {BrowserRouter} from 'react-router-dom'
-//import registerServiceWorker from './registerServiceWorker';
-//(at bottom) registerServiceWorker();
+import registerServiceWorker from './registerServiceWorker';
 
 render((<BrowserRouter>
     <Main/>
 </BrowserRouter>), document.getElementById('root'));
+registerServiceWorker();
