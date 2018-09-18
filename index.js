94dae5a0ee0aec14e7b5ca4722a4255b9af13d98
elburnie <lburneso@gmu.edu>
2018-09-18 12:37:57 -0400

    small changes

--- index.js
+++ index.js
@@ -228,7 +228,7 @@ app.get('/recipes/:postID/reviews', function(req, res) {
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
