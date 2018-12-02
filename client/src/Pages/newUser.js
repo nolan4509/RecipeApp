378aa761f72d41e90e82f49b003d36594cf21652
Nolan Meeks <nolanm4509@gmail.com>
2018-12-01 19:28:52 -0500

    Luke Here!  newUser now creates an entry in the user database

--- client/src/Pages/newUser.js
+++ client/src/Pages/newUser.js
@@ -1,46 +1,65 @@
 import React, {
-    Component
+  Component
 } from 'react';
 import firebase from '../firebase.js';
 import './styles.css';
 import SafeNavBar from '../Components/NavBar/NavBar';
 
 class newUser extends Component {
-    constructor(props) {
-        super(props)
-        this.handleChangeEmail = this.handleChangeEmail.bind(this)
-        this.handleChangePassword = this.handleChangePassword.bind(this)
-        this.state = {
-            email: '',
-            password: ''
+  constructor(props) {
+    super(props)
+    this.handleChangeEmail = this.handleChangeEmail.bind(this)
+    this.handleChangePassword = this.handleChangePassword.bind(this)
+    this.handleChangeUsername = this.handleChangeUsername.bind(this)
+    this.state = {
+      email: '',
+      password: '',
+      username: ''
+    }
+  }
+
+  handleChangeEmail(event) {
+    this.setState({
+      email: event.target.value
+    })
+  }
+
+  handleChangePassword(event) {
+    this.setState({
+      password: event.target.value
+    })
+  }
+
+  handleChangeUsername(event) {
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  createUser = (e) => {
+    e.preventDefault();
+    console.log(this.state.email);
+    firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then((UserCredential) => {
+      console.log('user data: ' + UserCredential.uid);
+      fetch(`/add/user/${this.state.username}/${UserCredential.uid}/${this.state.email}`, {
+        'method': 'POST',
+        'headers': {
+          'Content-Type': 'application/json'
         }
-    }
+      }).catch((error) => {
+        console.log('Error: ' + error);
+      });
+    }).catch(function(error) {
+      console.log(error.code);
+      console.log(error.message);
+    });
+    //    firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password);
+    this.props.history.push('/');
+  };
 
-    handleChangeEmail(event) {
-        this.setState({
-            email: event.target.value
-        })
-    }
-
-    handleChangePassword(event) {
-        this.setState({
-            password: event.target.value
-        })
-    }
-
-    createUser = (e) => {
-        e.preventDefault();
-        console.log(this.state.email);
-        firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).catch(function(error) {
-            console.log(error.code);
-            console.log(error.message);
-        })
-        this.props.history.push('/Home');
-    };
-
-    render() {
-        return (
-            <div className="backgroundStyle">
+  render() {
+    return (
+      <div className="backgroundStyle">
                 <SafeNavBar/>
                 <h3> New User </h3>
                 <form id = "newUserForm" onSubmit = {this.createUser}>
@@ -52,13 +71,17 @@ class newUser extends Component {
                         <label>Password</label> <br/>
                         <input type="text" name="userPasswordField" value={this.state.password} onChange={this.handleChangePassword}/>
                     </div>
+                    <div>
+                        <label>Username</label> <br/>
+                        <input type="text" name="userNameField" value={this.state.username} onChange={this.handleChangeUsername}/>
+                    </div>
                     <br/>
                         <button id="newUserButton" form="newUserForm" type="submit">Submit</button>
                     <br/>
                 </form>
             </div>
-        );
-    }
+    );
+  }
 }
 
 export default newUser;
