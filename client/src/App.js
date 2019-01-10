7abf0e72c395cf1057188cfab20521e5f58e1841
nolan4509 <nolanm4509@gmail.com>
2019-01-10 17:28:44 -0500

    Fixed App to realize home was deleted

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import {
     Switch,
     Route
 } from 'react-router-dom';
-import Home from './Pages/home';
 import Login from './Pages/login';
 import NewRecipe from './Pages/NewRecipePage';
 import Recipes from './Pages/RecipesPage';
@@ -12,7 +11,6 @@ import RecipeView from './Components/RecipeView/RecipeView';
 const App = () => (<main>
     <Switch>
         <Route exact={true} path='/' component={Login}/>
-        <Route path='/home' component={Home}/>
         <Route path='/NewRecipe' component={NewRecipe}/>
         <Route path='/Recipes' component={Recipes}/>
         <Route path='/Recipes/view' component={RecipeView}/>
