28845c84c70dcbaadb51c20566dca7dc620a8125
nolan4509 <nolanm4509@gmail.com>
2019-01-23 16:21:21 -0500

    Attempting to make AllRecipes.js not needed by moving the codein itspage

--- client/src/Pages/AllRecipesPage.js
+++ client/src/Pages/AllRecipesPage.js
@@ -1,3 +1,4 @@
+/*
 import React, {
     Component
 } from 'react';
@@ -16,6 +17,77 @@ class AllRecipesPage extends Component {
         return (<div className = "bodyStyle">
             <br/>
             <AllRecipes recipes={this.state.recipes} onView={this.handleViewRecipe.bind(this.state.recipes.recipeID)}/>
+        </div>);
+    }
+}
+
+export default AllRecipesPage;
+*/
+
+import React, {
+    Component
+} from 'react';
+// import AllRecipes from '../Components/AllRecipes/AllRecipes';
+import Recipes from '../Components/Recipes/Recipes';
+
+class AllRecipesPage extends Component {
+
+    constructor(props) {
+        super(props)
+        this.handleViewRecipe = this.handleViewRecipe.bind(this);
+        this.state = {
+            recipesLoaded: 'False',
+            recipes: [],
+            currentRecipe: 'Test'
+        }
+    }
+
+    getRecipes() {
+        fetch(`/recipes`, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }).then(res => res.json()).then((result) => {
+            console.log('Success: ' + result);
+            this.setState({
+                recipesLoaded: 'True',
+                recipes: result
+            })
+        }).catch((error) => {
+            console.log('In AllRecipes.js -- Error: ' + error);
+        });
+    }
+
+    handleViewRecipe(id) {
+        fetch(`/recipes/${id}`, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }).then(res => res.json()).then((result) => {
+            console.log('Success: ' + result);
+            // this.setState({
+            //     currentRecipe: result
+            // })
+        }).catch((error) => {
+            console.log('Error: ' + error);
+        });
+        // console.log(this);
+        this.props.history.push('/recipes/view');
+    }
+
+    componentDidMount() {
+        console.log('AllRecipesPage component mounted --');
+        this.getRecipes();
+    }
+
+    render() {
+        return (<div className = "bodyStyle">
+            <br/>
+            <Recipes recipes={this.state.recipes} onView={this.handleViewRecipe.bind(this.state.recipes.recipeID)}/>
         </div>);
     }
 }
